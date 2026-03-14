2b049f11dd3af0ff64a699fd1a27237cdadc0c5e
abhinav <abhinav01.ak@gmail.com>
2026-03-15 00:45:47 +0530

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,22 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
+import { BriefcaseBusiness } from "lucide-react";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Abhinav Kumar | Frontend Developer',
-  description: 'Frontend Developer with experience in Next.js, TypeScript, React, and Tailwind CSS. Building production-ready web applications.',
-  generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
-}
+  title: "Abhinav Kumar | Software Developer",
+  description:
+    "Software Developer with experience in Next.js, TypeScript, React, and Tailwind CSS. Building production-ready web applications.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -41,5 +25,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
